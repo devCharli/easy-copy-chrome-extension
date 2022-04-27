635224da2ce0f9ff4848516806117258e3c744a3
Charli <charlisung@gmail.com>
2022-04-27 14:27:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { FaTrashAlt, FaSave, FaRegCopy } from "react-icons/fa";
+import "./index.css";
 
 function App() {
+  const [list, setList] = useState(() => {
+    const savedList = localStorage.getItem("list");
+    if (savedList) {
+      return JSON.parse(savedList);
+    } else {
+      return [];
+    }
+  });
+
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input) {
+      return;
+    } else {
+      const newItem = {
+        id: uuidv4(),
+        text: input,
+      };
+      setList([...list, newItem]);
+      setInput("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const newList = list.filter((item) => item.id !== id);
+
+    setList(newList);
+  };
+
+  const handleCopy = (text, e) => {
+    navigator.clipboard.writeText(text);
+    e.target.className = "text clicked";
+    setTimeout(() => {
+      e.target.className = "text";
+    }, 500);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <h1>
+          copy <FaRegCopy />
+        </h1>
+        {/* add form */}
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Keywords..."
+            onChange={(e) => {
+              setInput(e.target.value.trim());
+            }}
+            value={input}
+          />
+          <button className="save-button">
+            <FaSave className="save-btn" />
+          </button>
+        </form>
+        {/* list */}
+        <ul>
+          {list.map((item) => (
+            <li className="list" key={item.id}>
+              <span
+                className="text"
+                onClick={(e) => {
+                  handleCopy(item.text, e);
+                }}
+              >
+                {" "}
+                {item.text}
+              </span>
+              <FaTrashAlt
+                className="trash"
+                onClick={() => handleDelete(item.id)}
+              ></FaTrashAlt>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
